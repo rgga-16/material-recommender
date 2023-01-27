0363770e9ce9007c62bfaae1c54fbb8c72710235
rgga16 <rgga16@gmail.com>
2023-01-27 16:18:18 +0900

    testing chatgpt

--- models/llm/chatgpt.py
+++ models/llm/chatgpt.py
@@ -5,12 +5,14 @@ References:
 - https://github.com/mmabrouk/chatgpt-wrapper
 '''
 
+import sys 
+
 from chatgpt_wrapper import ChatGPT
 
 bot = ChatGPT()
 # return the full result
-response = bot.ask("What are tables made of? Answer in bullet points.")
-print(response)
+# response = bot.ask("What are tables made of? Answer in bullet points.")
+# print(response)
 
 # return the result in streaming (chunks)
 # for chunk in bot.ask_stream("tell me a story about cats and dogs"):
