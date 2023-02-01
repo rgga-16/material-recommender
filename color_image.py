b75f3ca502733408ad799c8ef7cfd8978ffbbce6
rgga16 <rgga16@gmail.com>
2023-02-01 17:09:29 +0900

    color_image function. May need to use later.

---
+++ color_image.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os 
+
+def apply_color(image_path, color_hex:str,alpha=0.5):
+    color_hex = color_hex.lstrip('#')
+    # Open the input image
+    img = Image.open(image_path)
+    
+    # Convert the input image to RGBA
+    img = img.convert("RGBA")
+    
+    # Get the RGBA values of the input color
+    color = tuple(int(color_hex[i:i+2], 16) for i in (0, 2, 4)) + (255,)
+    
+    # Create a new image with the same size and mode as the input image
+    new_img = Image.new("RGBA", img.size, color)
+    
+    # Blend the input image and the new image using the "over" blending mode
+    blended_img = Image.blend(new_img, img, alpha=alpha)
+    
+    out_path = f"{os.path.splitext(os.path.basename(image_path))[0]}_out.png"
+    # Save the blended image
+    blended_img.save(out_path)
+    
+# Example usage
+if __name__=="__main__":
+    apply_color("./tmp/dark teak.png", "#d56062",alpha=0.5)
