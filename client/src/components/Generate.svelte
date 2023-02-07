42275f97c1810cb9de155dba2749ad6eade38508
rgga16 <rgga16@gmail.com>
2023-02-07 17:50:53 +0900

    Added function to generate textures; Added function to apply them onto selected model parts

--- client/src/components/Generate.svelte
+++ client/src/components/Generate.svelte
@@ -1,25 +1,89 @@
 <script>
     import {onMount} from 'svelte';
+    import GeneratedTextures from './GeneratedTextures.svelte';
 
     let input_material='';
-    let object_data={}; //List of names of 3D objects 
+    let selected_object_parts=[]; 
 
-    let objs_and_parts = fetch('./get_objects_and_parts').then((x)=>x.json());
+    const objs_and_parts = fetch('./get_objects_and_parts').then((x)=>x.json());
 
-    function generate_textures() {
-        // Do something with input_material
+    // let gen_texture_paths = [];
+    // let rendering_paths = [];
+    let rendering_texture_pairs = [];
+
+    function handleCheckboxChange(event) {
+        const checkbox = event.target; 
+        checkbox.checked = event.target.checked;
     }
 
-    function apply_textures() {
+    async function gen_and_apply_textures(texture_str) {
+
+        let selected_op_dict = {};
+
+        if (selected_object_parts.length <= 0) { alert("Please select at least 1 object part"); return }
+
+        // Insert algo to parse the selected_object_parts
+        for (let i = 0; i < selected_object_parts.length; i++) {
+            let splitted = selected_object_parts[i].split("-");
+            let obj = splitted[0];
+            let part = splitted[1];
+            if(obj in selected_op_dict) {
+                selected_op_dict[obj].push(part);
+            } else {
+                selected_op_dict[obj] = [part]; 
+            }
+        }
         
+        const results_response = await fetch("/generate_and_transfer_textures", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "texture_string": texture_str,
+                "n":4,
+                "imsize":256,
+                "obj_parts_dict": selected_op_dict,
+            }),
+        });
+        const results_json = await results_response.json();
+        rendering_texture_pairs = results_json["results"];
     }
     
- 
+    // async function generate_textures(texture_str) {
+    //     // Do something with input_material
+    //     const gen_textures_response = await fetch("/generate_textures", {
+    //         method: "POST",
+    //         headers: {"Content-Type": "application/json"},
+    //         body: JSON.stringify({
+    //             "texture_string": texture_str,
+    //             "n":4,
+    //             "imsize":256
+    //         }),
+    //     });
+    //     const gen_textures_json = await gen_textures_response.json();
+    //     gen_texture_paths = gen_textures_json["generated_textures"]
+    // }
+
+    // async function apply_textures(texture_str, selected_obj_parts, texture_paths) {
+    //     request_json = {
+    //         "texture_string": texture_str,
+    //         "obj_parts_dict": selected_obj_parts,
+    //         "texture_paths": texture_paths,
+    //     }
+        
+    //     const renderings_response = await fetch("/transfer_textures", {
+    //         method: "POST",
+    //         headers: {"Content-Type": "application/json"},
+    //         body: JSON.stringify(request_json),
+    //     });
+    //     const renderings_json = await renderings_response.json();
+    // }
+
 </script>
 
 <div class="material_generator">
     <h2> Material Generator </h2>
-    <form on:submit|preventDefault={generate_textures}>
+    <!-- <form on:submit|preventDefault={generate_textures(input_material)}> -->
+    <form on:submit|preventDefault={gen_and_apply_textures(input_material)}>
         <input name="material_name" type="text" bind:value={input_material} required/>
         <br/>
         <div class="tab-group">
@@ -27,13 +91,13 @@
             {#await objs_and_parts}
                 <pre>Loading object names and their part names</pre>
             {:then data} 
-                {#each Object.entries(data) as [obj,attribs]}
+                {#each Object.entries(data) as [obj_name,attribs]}
                     <div class="tab">
-                        <input type="radio" name="css-tabs" id="tab-{obj}" checked="checked" class="tab-switch">
-                        <label for="tab-{obj}" class="tab-label">{obj}</label>
+                        <input type="radio" name="css-tabs" id="tab-{obj_name}" checked="checked" class="tab-switch">
+                        <label for="tab-{obj_name}" class="tab-label">{obj_name}</label>
                         <div class="tab-content">
                             {#each attribs.parts.names as part_name}
-                                <input type="checkbox" id="checkbox-{part_name}" name="checkbox-group-{obj}">
+                                <input type="checkbox" bind:group={selected_object_parts} id="checkbox-{obj_name}-{part_name}" name="checkbox-group-{obj_name}" value="{obj_name}-{part_name}" >
                                 <label for="checkbox-{part_name}"> {part_name} </label>
                             {/each}
                         </div>
@@ -44,9 +108,14 @@
         <br/>
         <button> Generate & Transfer Material </button>
     </form>
-    <form>
+    {#if rendering_texture_pairs}
+        <form>
+            <GeneratedTextures rendering_texture_pairs= {rendering_texture_pairs} />
+        </form>
+    {/if}
+    
         <!-- This form shows a preview of up to 4 generated textures applied to the parts of the 3D models -->
-    </form>
+    
 
 </div>
 
@@ -55,7 +124,7 @@
 <style>
     .tab-group {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         align-items: center;
     }
     input[type="radio"] {
--- server.py
+++ server.py
@@ -1,22 +1,129 @@
-from flask import Flask, send_from_directory
-import random, requests, json
+from flask import Flask, send_from_directory, request, jsonify
+import random, requests, json, copy 
+from PIL import Image
+
+from texture_transfer_3d import TextureDiffusion
 
 import os 
+
 '''
 Tutorial on how to use Svelte to fetch APIs 
 https://sveltesociety.dev/recipes/component-recipes/using-fetch-to-consume-apis
 '''
 
+CWD = os.getcwd()
+SERVER_IMDIR = os.path.join(CWD,"client","public","gen_images")
+CLIENT_IMDIR = os.path.join("gen_images")
+CURR_RENDER_ID = 1
 
 products = [
     "nightstand_family",
 ]
 
 data_dir = os.path.join(os.getcwd(),"data","3d_models",products[0])
+renderings_dir = os.path.join(data_dir,"renderings")
+
 rendering_setup_path = os.path.join(data_dir,"rendering_setup.json")
 
+init_texture_parts = json.load(open(os.path.join(renderings_dir, str(CURR_RENDER_ID),"object_part_material.json")))
+current_texture_parts = copy.deepcopy(init_texture_parts)
 
 app = Flask(__name__, static_folder="./client/public")
+
+# @app.route("/transfer_textures", methods=['POST'])
+# def transfer_textures():
+#     form_data = request.get_json()
+
+#     texture_string = form_data["texture_string"]
+#     obj_part_dict = form_data["obj_parts_dict"]
+#     texture_paths = form_data["texture_paths"]
+
+#     rendering_paths = []
+
+#     for i in range(len(texture_paths)):
+#         new_texture_parts = copy.deepcopy(current_texture_parts)
+#         for obj in list(obj_part_dict.keys()):
+#             for part in obj_part_dict[obj]:
+#                 new_texture_parts[obj][part]["mat_name"]=texture_string
+#                 new_texture_parts[obj][part]["mat_finish"]="glossy"
+#                 new_texture_parts[obj][part]["mat_image_texture"]=texture_paths[i]
+                
+#         tmp_texture_parts_savepath = os.path.join(SERVER_IMDIR,"renderings",f"texture_parts_{i}.json")
+        
+#         with open(tmp_texture_parts_savepath,"w") as tmpfile:
+#             json.dump(new_texture_parts,tmpfile)
+
+#         tmp_rendering_savepath = os.path.join(SERVER_IMDIR,"renderings",f"rendering_{i}.png")
+#         tmp_rendering_loadpath = os.path.join(CLIENT_IMDIR,"renderings",f"rendering_{i}.png")
+#         rendering_paths.append(tmp_rendering_loadpath)
+#         command_str = f'blender --background --python render_obj_and_textures.py -- --out_path {tmp_rendering_savepath} --rendering_setup_json {rendering_setup_path} --texture_object_parts_json {tmp_texture_parts_savepath}'
+#         os.system(command_str)
+
+#     return rendering_paths
+
+@app.route("/generate_and_transfer_textures", methods= ['POST'])
+def generate_and_transfer_textures():
+    form_data = request.get_json()
+
+    texture_string = form_data["texture_string"]
+    n = form_data["n"]
+    imsize = form_data["imsize"]
+    obj_part_dict = form_data["obj_parts_dict"]
+
+    #################### Generating the textures ################
+
+    textures, filenames = generate_textures(texture_string, n, imsize)
+
+    #################### Transferring the textures ##############
+
+    rendering_texture_pairs = []
+
+    for i in range(len(textures)):
+
+        textures[i].save(os.path.join(SERVER_IMDIR,filenames[i]))
+        texture_loadpath = os.path.join(CLIENT_IMDIR,filenames[i])
+
+        new_texture_parts = copy.deepcopy(current_texture_parts)
+        for obj in list(obj_part_dict.keys()):
+            for part in obj_part_dict[obj]:
+                new_texture_parts[obj][part]["mat_name"]=texture_string
+                new_texture_parts[obj][part]["mat_finish"]="glossy"
+                new_texture_parts[obj][part]["mat_image_texture"]=os.path.join(SERVER_IMDIR,filenames[i])
+                
+        tmp_texture_parts_savepath = os.path.join(SERVER_IMDIR,"renderings",f"texture_parts_{i}.json")
+        
+        with open(tmp_texture_parts_savepath,"w") as tmpfile:
+            json.dump(new_texture_parts,tmpfile)
+
+        rendering_savepath = os.path.join(SERVER_IMDIR,"renderings",f"rendering_{i}.png")
+
+        rendering_loadpath = os.path.join(CLIENT_IMDIR,"renderings",f"rendering_{i}.png")
+
+        # rendering_loadpaths.append(rendering_loadpath)
+        command_str = f'blender --background --python render_obj_and_textures.py -- --out_path {rendering_savepath} --rendering_setup_json {rendering_setup_path} --texture_object_parts_json {tmp_texture_parts_savepath}'
+        os.system(command_str)
+
+        rendering_texture_pairs.append(
+            {'rendering':rendering_loadpath, 'texture':texture_loadpath}
+        )
+
+    return {"results": rendering_texture_pairs}
+
+def generate_textures(texture_string, n, imsize):
+
+    generated_textures = texture_generator.text2texture(texture_string, n=n, gen_imsize=imsize)
+    texture_filenames = []
+
+    for i in range(len(generated_textures)):
+        texture_filenames.append(f"{texture_string}_{i}.png")
+        
+        # server_texture_path = os.path.join(SERVER_IMDIR,f"{texture_string}_{i}.png")
+        # generated_textures[i].save(server_texture_path)
+        # client_texture_path = os.path.join(CLIENT_IMDIR,f"{texture_string}_{i}.png")
+        # client_texture_paths.append(texture_loadpath)
+
+    return generated_textures, texture_filenames
+
 
 @app.route("/get_objects_and_parts")
 def load_object_data():
@@ -35,8 +142,5 @@ def home(path):
     return send_from_directory('./client/public', path)
 
 if __name__ == "__main__":
-    
-    
-    
-
+    texture_generator = TextureDiffusion()
     app.run(debug=True)
