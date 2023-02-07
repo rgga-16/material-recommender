6037a03f6090c10af487acb37f5da00fc84eee22
rgga16 <rgga16@gmail.com>
2023-02-07 17:51:22 +0900

    Added components to display renderings and generated textures (WIP)

---
+++ client/src/components/GeneratedTextures.svelte
@@ -0,0 +1,26 @@
+
+<script>
+    export let rendering_texture_pairs;
+</script>
+
+<div class="image-grid">
+    {#each rendering_texture_pairs as pair}
+        <img class="grid-item" src={pair.rendering} alt=""/>
+        <img class="grid-item" src={pair.texture} alt=""/>
+    {/each} 
+</div>
+
+
+<style>
+    .image-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        grid-gap: 10px;
+    }
+
+    .grid-item {
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+    }
+</style>
