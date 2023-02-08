251952556bab041b999a10adfd3ff757013880dc
rgga16 <rgga16@gmail.com>
2023-02-08 17:31:53 +0900

    Made generated textures selectable

--- client/src/components/PreviewCard.svelte
+++ client/src/components/PreviewCard.svelte
@@ -1,7 +1,36 @@
 <script>
+    export let texture;
+    export let rendering; 
+    export let info; 
+    export let index;
 
+    let currentImage=0;
+    let viewString="View texture";
+    let is_selected=false;
+
+    function switchImage() {
+        if(currentImage===0) {
+            currentImage = 1;
+            viewString = "View rendering"
+        } else {
+            currentImage = 0;
+            viewString = "View texture"
+        }
+    }
 </script>
 
+{#if currentImage===0}
+    <img src ={rendering} alt = "Rendering" />
+{:else} 
+    <img src ={texture} alt = "Texture" />
+{/if}
+<button on:click|preventDefault={switchImage}> {viewString} </button>
+
 <style>
+    img{
+        width:100%;
+        object-fit: cover;
+    }
+
     
 </style>
