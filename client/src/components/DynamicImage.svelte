e3a8b0d58d615ecdc13ae9c5f71756df664ed116
rgga16 <rgga16@gmail.com>
2023-05-17 14:43:09 +0900

    drag and drop generated texture (WIP)

--- client/src/components/DynamicImage.svelte
+++ client/src/components/DynamicImage.svelte
@@ -1,11 +1,13 @@
 <script>
 import { onMount } from "svelte";
 import { Circle } from 'svelte-loading-spinners';
+import {transferred_texture_url} from '../stores.js';
 
 // This component is a dynamic image component. It should dynamically load an image given its path.
 export let imagepath; //Image path that will be passed to the server to get the image
 export let alt; //Alternate text to be displayed
 export let size="200px";
+export let is_draggable=false;
 let imagesource; //Returned image
 
 let is_loading=false;
@@ -28,6 +30,16 @@ export async function getImage() {
     }).catch(error => console.error(error));
 }
 
+function dragStart(event) {
+    if (!is_draggable) {
+        event.preventDefault();
+        return;
+    }
+    // console.log("dragging!" + event.target.src);
+    event.dataTransfer.setData("text/plain", event.target.src);
+    transferred_texture_url.set(event.target.src);
+}
+
 $: getImage();
 
 onMount(getImage);
@@ -38,7 +50,7 @@ onMount(getImage);
     {#if is_loading}
         <Circle size="60" color="#FF3E00" unit="px" duration="1s" />
     {:else}
-        <img src={imagesource} alt={alt ? alt:"Image"} style="max-width: {size}; max-height:{size}">
+        <img src={imagesource} alt={alt ? alt:"Image"} style="max-width: {size}; max-height:{size}" draggable={is_draggable} on:dragstart={dragStart}>
         <!-- <img src={imagesource} alt={alt ? alt:"Image"}>  -->
     {/if}
 
