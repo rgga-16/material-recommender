d442e5fc9f63209f0bb0bf302f489b89835824ee
rgga16 <rgga16@gmail.com>
2023-12-08 17:26:24 +0900

    stuff

--- client/src/components/ChatBotModule/ChatBot.svelte
+++ client/src/components/ChatBotModule/ChatBot.svelte
@@ -252,7 +252,7 @@
     }
 
     onMount(async () => { //UNCOMMENT ME WHEN YOU'RE TESTING THE CHATBOT
-        await init_query();
+        // await init_query();
         // If the chatbot_input_message, a global store, is updated, update the inputMessage variable and the text in the textbox message area.
         chatbot_input_message.subscribe(value => {
             inputMessage = value;
