f85785435cc4baf7c64b9102d22d441faa9322fe
rgga16 <rgga16@gmail.com>
2024-08-29 18:01:39 +0900

    debugged

--- other/krippendorff_alpha.py
+++ other/krippendorff_alpha.py
@@ -4,14 +4,17 @@ import numpy as np
 import krippendorff
 
 # Specify the file path
-dir = './experiment_data/Expert Validation/Krippendorff Alpha/'
+dir = './experiment_data/Expert Validation/Krippendorff Alpha'
 file_path = os.path.join(dir, 'Use and Safety.csv')
 
 # Read the .csv file into a pandas dataframe
-data = pd.read_csv(file_path)
+orig_data = pd.read_csv(file_path)
 
-# Remove the first column and the first row from data
-data = data.iloc[3:, 1:]
+# # Remove the first column and the first row from data
+# data_no_exp1 = orig_data.iloc[1:] #Remove the first row (Expert #1)
+
+
+data = orig_data.iloc[1:, 1:] #Remove the first row (Expert #1) and the first column (Email)
 data = data.replace('Unsuitable', 1)
 data = data.replace('Somewhat unsuitable', 2)
 data = data.replace('Unsure', 3)
@@ -26,4 +29,4 @@ data_np = np.array(data_list)
 
 alpha = krippendorff.alpha(data_np, level_of_measurement='ordinal')
 print(f"Krippendorff's alpha: {alpha}")
-
+pass
